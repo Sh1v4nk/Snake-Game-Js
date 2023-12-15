0e7696a1f984237b0bd8a6adc10e43e05157d8e6
Sh1v4nk <shivankpandey113@gmail.com>
2023-12-15 22:11:44 +0530

    Implimented some initial Js logic for game

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("Hello World");
+const startScreen = document.querySelector(".start-screen");
+const startBtn = document.querySelector(".start-button");
+const scores = document.querySelector(".scores");
+const currentScoreDisplay = document.querySelector("#current-score");
+const highScoreDisplay = document.querySelector("#high-score");
+const gameArea = document.querySelector(".game-container");
+
+let currentScore = 0;
+let highScore = 0;
+
+let playGame = true;
+
+if (playGame) {
+  startBtn.addEventListener("click", startGame);
+}
+
+function startGame() {
+  startScreen.style.display = "none";
+  scores.style.display = "flex";
+  gameArea.style.display = "block";
+  draw();
+}
+
+function draw() {
+  const snakeDiv = document.createElement("div");
+  snakeDiv.classList.add("snake");
+
+  const foodDiv = document.createElement("div");
+  foodDiv.classList.add("food");
+  
+  gameArea.appendChild(snakeDiv);
+  gameArea.appendChild(foodDiv);
+}
